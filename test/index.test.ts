b5fc5210cf6aabf71fd10ab87e11994a1ccb9624
Ian Harris <ianwebsterharris@gmail.com>
2019-11-28 23:18:40 -0700

    Tests

---
+++ test/index.test.ts
@@ -0,0 +1,36 @@
+import { Timer, HRTime } from '../src/index';
+
+describe('Timer', () => {
+    let hrtimeSpy: jest.SpyInstance;
+
+    beforeEach(() => {
+        hrtimeSpy = jest.spyOn(process, 'hrtime');
+    });
+
+    afterEach(() => {
+        hrtimeSpy.mockClear();
+    });
+
+    it('should start the timer upon construction', () => {        
+        const timer = new Timer();
+
+        hrtimeSpy.mockImplementationOnce(() => [123, 456]);
+
+        expect(hrtimeSpy).toHaveBeenCalledTimes(1);
+        expect(hrtimeSpy).toHaveBeenLastCalledWith<[]>();
+        expect(timer.elapsed()).toEqual([123, 456]);
+    });
+
+    it('should calculate elapsed time', () => {
+        const timer = new Timer();
+
+        hrtimeSpy.mockImplementation(() => [123, 123456789]);
+
+        expect(timer.elapsedSeconds()).toBe(123);
+        expect(timer.elapsedMilliseconds()).toBe(123123);
+        expect(timer.elapsedNanoseconds()).toBe(123123456789);
+        expect(hrtimeSpy).toHaveBeenCalledTimes(4);
+
+        
+    });
+});
